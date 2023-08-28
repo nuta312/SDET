6fdc331f2427afeb1f8c46ba1dcf43083e1d654d
aidar61 <aidar310780@gmail.com>
2023-08-28 21:42:33 +0600

    user crt

--- api/src/test/java/com/digitalnomads/apiTests/users/UserGetTest.java
+++ api/src/test/java/com/digitalnomads/apiTests/users/UserGetTest.java
@@ -4,16 +4,24 @@ import com.digitalnomads.api.entities.User;
 import com.digitalnomads.apiTests.BaseApiTest;
 import org.testng.annotations.Test;
 
+import java.util.Arrays;
+
 import static org.testng.Assert.assertEquals;
 
 public class UserGetTest extends BaseApiTest {
     @Test
-    public void getUsers() {
+    public void getUserByEmail() {
         String expectedEmail = "example23@example.com";
         String expectedEmailNegative = "example23@example.com1";
         userController.getUserByEmail(expectedEmail);
         User user = userController.extractObjectFromResponse(User.class);
         System.out.println(user.toString());
         assertEquals(user.getEmail(), expectedEmailNegative, "User email is not correct");
+    }
+
+    @Test
+    public void getUsers() {
+        User[] user = userController.getAllUsers().as(User[].class);
+        System.out.println(Arrays.toString(user));
     }
 }
